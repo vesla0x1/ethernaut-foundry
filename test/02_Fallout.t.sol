2042fb2606ed7986957b879353003a726a0892f5
vesla0x1 <vesla0x1@gmail.com>
2025-02-18 23:37:12 -0300

    feat: add fallout

---
+++ test/02_Fallout.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Fallout} from "../src/02_Fallout.sol";
+
+contract TestFallout is Test {
+    address public player = address(0xbad);
+    Fallout public instance;
+
+    function setUp() public {
+        instance = new Fallout();
+        vm.deal(player, 0.001 ether);
+    }
+
+    function testSolution() public {
+        vm.startPrank(player);
+
+        assertEq(instance.owner(), player);
+
+        vm.stopPrank();
+    }
+}
