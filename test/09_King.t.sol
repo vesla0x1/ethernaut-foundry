456e30ba9e71b8738079f5f07579b6598f1bac97
vesla0x1 <vesla0x1@gmail.com>
2025-02-19 12:29:42 -0300

    feat: add king

---
+++ test/09_King.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {King} from "../src/09_King.sol";
+
+contract TestKing is Test {
+    address public player = address(0xbad);
+    address public bob = address(0xb0b);
+    King public instance;
+
+    function setUp() public {
+        vm.deal(player, 0.001 ether);
+        vm.deal(bob, 0.001 ether);
+
+        vm.prank(bob);
+        instance = new King{value: 1}();
+    }
+
+    function testSolution() public {
+        assertEq(instance._king(), address(bob));
+        vm.startPrank(player);
+
+        // Your exploit goes here
+
+        vm.stopPrank();
+
+        (bool success,) = payable(address(instance)).call{value: instance.prize()}("");
+        assertFalse(success);
+        assertNotEq(instance._king(), address(bob));
+    }
+}
