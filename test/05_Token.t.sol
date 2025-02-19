768d139b730f3a0d01c1baf217e1f165f06a826f
vesla0x1 <vesla0x1@gmail.com>
2025-02-19 00:37:05 -0300

    feat: add Token

---
+++ test/05_Token.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Token} from "../src/05_Token.sol";
+
+contract TestToken is Test {
+    address public player = address(0xbad);
+    address public bob = address(0xb0b);
+    Token public instance;
+    uint256 public constant initialPlayerBalance = 20;
+
+    function setUp() public {
+        vm.deal(player, 0.001 ether);
+        vm.deal(bob, 0.001 ether);
+
+        vm.startPrank(bob);
+        instance = new Token(100);
+        instance.transfer(player, initialPlayerBalance);
+        vm.stopPrank();
+    }
+
+    function testSolution() public {
+        vm.startPrank(player);
+
+        assertGt(instance.balanceOf(player), initialPlayerBalance);
+
+        vm.stopPrank();
+    }
+}
