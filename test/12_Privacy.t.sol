eefb07e2b9da02f295de5a2ba294661b05b967ec
vesla0x1 <vesla0x1@gmail.com>
2025-02-19 16:39:14 -0300

    feat: add privacy

---
+++ test/12_Privacy.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Privacy} from "../src/12_Privacy.sol";
+
+contract TestPrivacy is Test {
+    address public player = address(0xbad);
+    Privacy public instance;
+
+    function setUp() public {
+        vm.deal(player, 0.001 ether);
+
+        bytes32[3] memory data;
+        for (uint8 i = 0; i < 3; i++) {
+            data[i] = bytes32(vm.randomBytes(32));
+        }
+
+        instance = new Privacy(data);
+    }
+
+    function testSolution() public {
+        vm.startPrank(player);
+
+        // Your exploit goes here
+
+        vm.stopPrank();
+        assertFalse(instance.locked());
+    }
+}
