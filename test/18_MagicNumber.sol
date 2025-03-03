55d7b5ff7c543c83b899a490018f9774d29bd1ec
vesla0x1 <vesla0x1@gmail.com>
2025-03-03 13:06:11 -0300

    feat: add MagicNumber solution

--- test/18_MagicNumber.sol
+++ test/18_MagicNumber.sol
@@ -21,7 +21,33 @@ contract TestMagicNum is Test {
         address solver;
         vm.startPrank(player);
 
-        // Your exploit goes here
+        // PUSH1 42: 60 2a
+        // PUSH1 0: 60 00
+        // MSTORE: 52
+        // PUSH1 32: 60 20 
+        // PUSH1 0: 60 00
+        // RETURN: F3 offset, size 
+
+        // runtime bytecode: 602a60005260206000f3
+
+        // PUSH1 10: 60 0a
+        // PUSH1 XX: 60 XX -> 0c (this initialization bytecode has 12 bytes)
+        // PUSH1 00: 60 00
+        // CODECOPY: 39 <destOffset> <offset> <size>
+        // PUSH1 10: 60 0a
+        // PUSH1 0:  60 00
+        // RETRUN: f3 offset, size
+
+        // initialization bytecode: 600a600c600039600a6000f3
+
+        // final bytecode: 600a600c600039600a6000f3602a60005260206000f3
+        assembly {
+            let ptr := mload(0x00)
+            mstore(ptr, shl(0x50, 0x600a600c600039600a6000f3602a60005260206000f3))
+            solver := create(0, ptr, 0x16)
+        }
+
+        instance.setSolver(solver);
 
         vm.stopPrank();
 
